6761032b310e18dfc17549dd9f3e016988e19b8c
vent-lab <cfpixel12@gmail.com>
2020-09-17 22:37:03 +0100

    Setup basic functionality, added file input and output

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 import requests
-def main():
-    while True:
-        try:
-            print("Welcome to my URL Fuzzer")
-            url = input("What is your URL?")
-            wordlist = input("What is the file for your wordlist?")
-            threadCount = input("How many threads (1-500)?")
-            r = requests.get(url+wordlist, stream=True)
+
+print("Welcome to my URL Fuzzer")
+url = input("What is your URL? ")
+wl = input("What is the file for your wordlist? ")
+threadCount = input("How many threads (1-500)? ")
+
+def main(url,wl,threadCount):
+    with open(wl, 'r') as file:
+        for line in file.read().split('\n'):
+            r = requests.get(url+line)
             if(r.ok):
+                print("Success")
                 print(r.url)
-                print(r.status_code)
-                print("Success!")
             else:
-                print(r.status_code)
-                print("Failure!")
-                return 0
-        except:
-            print("Error! Please try again.")
-            main()
+                print("Fail")
+                print(r.url)
 
-        
-main()
-
+main(url,wl,threadCount)
