38b48bcd102171fbb55db16efbccdbf8fcd796b7
vent-lab <cfpixel12@gmail.com>
2020-09-20 22:57:10 +0100

    Cleaned up code, introduced comments into the program

--- main.py
+++ main.py
@@ -4,31 +4,45 @@ import threading
 import concurrent.futures
 from time import perf_counter
 
+#Initilized an argument parser
 parser = argparse.ArgumentParser(prog='fuzzer', description='A tool for finding hidden files and directories on a web server.')
-parser.add_argument('--url', dest='url', action='store', 
-help="Used to parse a valid URL for fuzzing")
-parser.add_argument('--wordlist', dest='wordlist', action='store', 
-help="Used to parse wordlist file for fuzzing")
-parser.add_argument('--threads', dest='threads', type=int, action='store',
-help="Used to request a specific number of threads to be used")
-args = parser.parse_args()
 
+#Created a seperate argument group
+requiredNamed = parser.add_argument_group('required named arguments')
+
+#Invoked required arguments
+requiredNamed.add_argument('-url', dest='url', action='store', required=True, 
+help="Used to parse a valid URL for fuzzing")
+requiredNamed.add_argument('-wordlist', dest='wordlist', action='store', required=True,
+help="Used to parse wordlist file for fuzzing")
+requiredNamed.add_argument('-threads', dest='threads', type=int, action='store', required=True,
+help="Used to request a specific number of threads to be used")
+
+#Parsed arguments to the args variable
+args = parser.parse_args(['-h'])
+
+#Created a thread pool with the target of the Fuzzer function
 def main(args):
     try:
-        start = perf_counter()
-        with open(args.wordlist, 'r') as file:
-            for line in file.read().split('\n'):
-                r = requests.get(args.url+line)
-                if(r.ok):
-                    print(r.url)
-                else:
-                    continue
+        with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads,) as executor:
+            executor.map(Fuzzer(args), range(args.threads))
     except:
+        print("Error!")
         return 0
 
-    end = perf_counter()
-    execution_time = end - start
-    print(f"Completed in {execution_time} seconds!")
+#Sends requests to a specified web server using paths from a file
+def Fuzzer(args):
+    startTime = perf_counter()
+    with open(args.wordlist, 'r') as file:
+        for line in file.read().split('\n'):
+            r = requests.get(args.url+line)
+            if(r.ok):
+                print(r.url)
+            else:
+                continue
+    endTime = perf_counter()
+    executionTime = endTime - startTime
+    print(f"Completed in %H:%M:%S", executionTime, " seconds!")
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads,) as executor:
-    executor.map(main(args), range(args.threads))
+#Invoked the main function, along with the arguments
+main(args)
